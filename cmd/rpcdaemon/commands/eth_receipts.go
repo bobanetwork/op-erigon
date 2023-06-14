b8167226410e556b7bb2b2941cd91ef732e48ce0
Michael Montour <mmontour@enya.ai>
2023-06-14 16:57:17 -0700

    Don't try to extract the ChainID from a Deposit transaction.  Changes to be committed: 	modified:   cmd/rpcdaemon/commands/eth_receipts.go

--- cmd/rpcdaemon/commands/eth_receipts.go
+++ cmd/rpcdaemon/commands/eth_receipts.go
@@ -755,6 +755,8 @@ func marshalReceipt(receipt *types.Receipt, txn types.Transaction, chainConfig *
 		chainId = t.ChainID.ToBig()
 		// case *types.SignedBlobTx: // TODO: needs eip-4844 signer
 		// 	chainId = t.GetChainID().ToBig()
+	case *types.DepositTransaction:
+		chainId = chainConfig.ChainID
 	default:
 		chainId = txn.GetChainID().ToBig()
 	}
